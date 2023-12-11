f3d3b3a1083c04ace9dce6645f5ce80e79fccbb0
Pietro <54339762+pietronr@users.noreply.github.com>
2023-12-11 18:13:12 -0300

    Add files via upload

---
+++ Challenges/PalindromeChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Classes.Challenges
+{
+    /// <summary>
+    /// Read a string from console and check if it's a palindrome.
+    /// </summary>
+    internal static class PalindromeChecker
+    {
+        static void Main()
+        {
+            string value = Console.ReadLine()!;
+            char[] inverse = new char[value.Length];
+
+            bool areEqual = true;
+
+            int lastIndex = value.Length - 1;
+
+            for (int i = 0; i < value!.Length; i++)
+            {
+                inverse[lastIndex - i] = value[i];
+
+                if (!inverse[lastIndex - i].Equals(value[lastIndex - i]))
+                {
+                    areEqual = false;
+                }
+            }
+
+            Console.WriteLine(inverse);
+
+            if (areEqual)
+            {
+                Console.WriteLine("É um palíndromo!");
+            }
+        }
+    }
+}
